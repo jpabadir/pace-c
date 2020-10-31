afd4a72cdd3cedfa272dffe284e3b306d4801a1d
Katrina Martel <k.martel@outlook.com>
2020-10-30 20:15:35 -0700

    modified environment to exclude extraneous info

--- src/front-end/src/App.js
+++ src/front-end/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 
 function callServer() {
@@ -9,21 +8,7 @@ function callServer() {
 function App() {
   return (
     <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <button onClick={callServer}>Call the server!</button>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={callServer}>Call the server!</button>
     </div>
   );
 }
