1c55dc9584652e3b62168b5d6da8e0733d5a0f82
Katrina Martel <k.martel@outlook.com>
2020-11-01 13:39:07 -0800

    modified input field and added date picker field

--- src/front-end/src/components/MentorshipForm.js
+++ src/front-end/src/components/MentorshipForm.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
 import { inputField } from "./Inputs";
-import { Form, Button, Input } from "antd";
+import { Form, Button, Input, DatePicker, Tooltip } from "antd";
+import { InfoCircleOutlined} from '@ant-design/icons';
+
+
+const { RangePicker } = DatePicker;
 
 
 class MentorshipForm extends Component {
@@ -12,8 +16,23 @@ class MentorshipForm extends Component {
                     name="testInput"
                     rules={[{ required: true, message: 'Please input something!' }]}
                 >
-                    <Input />
+                   <Input 
+                    placeholder = "Placeholder for test"
+                    prefix={
+                        <Tooltip title="This field will likely be deleted. I just wanted to test out this feature.">
+                          <InfoCircleOutlined style={{ color: 'rgba(0,0,0,.45)' }} />
+                        </Tooltip>
+                      }/>
                 </Form.Item>
+                <Form.Item 
+                    label = "Timeframe Seeking Mentorship"
+                    name = "selectedStartDate"
+                    rules={[{ required: true, message: 'Please pick an estimated duration for how long you would like to use the platform' }]}
+                    >
+                       <RangePicker 
+                       picker = "month" 
+                       />
+                    </Form.Item>
                 <Button type="primary" htmlType="submit">
                     Submit
         </Button>
