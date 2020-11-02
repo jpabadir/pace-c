b0d8aea072fa33f84fefdd548ceb5ba91f175185
Jean-Philippe Abadir <jpabadir@gmail.com>
2020-11-02 01:39:10 +0100

    Enable linting on repo, add tech directive, and do initial linting

---
+++ documentation/tech-directives/linting.md
@@ -0,0 +1,7 @@
+# Comprehensive list of the code standards the team follows
+
+###### When possible, IDE code-linting rule files will be made and shared with the team to facilitate code standardizing.
+
+### Front-end
+
+While in the src/front-end directory, run `eslint src/*.js --fix` to lint your code using the project's ESLint standard presets.
--- src/front-end/src/App.js
+++ src/front-end/src/App.js
@@ -1,25 +1,33 @@
+// import request from 'request';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function callServer() {
-  var request = require('request');
-  request('http://localhost:8000/', function (error, response, body) {});
+  // const request = require('request');
+  // request('http://localhost:8000/', (error, response, body) => {});
 }
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit
+          {' '}
+          <code>src/App.js</code>
+          {' '}
+          and save to reload.
         </p>
-        <button onClick={callServer}>Call the server!</button>
+        <button onClick={callServer} type="submit">
+          Call the server!
+        </button>
         <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
         >
           Learn React
         </a>
