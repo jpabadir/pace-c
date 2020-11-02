ae5daac2c77e89aa1ed17b44158b54ea1f8b9098
Katrina Martel <k.martel@outlook.com>
2020-11-01 18:01:09 -0800

    first iteration for selection of availability

--- src/front-end/src/components/MentorshipForm.js
+++ src/front-end/src/components/MentorshipForm.js
@@ -1,11 +1,19 @@
 import React, { Component } from "react";
 import { inputField } from "./Inputs";
-import { Form, Button, Input, DatePicker, Tooltip } from "antd";
-import { InfoCircleOutlined} from '@ant-design/icons';
+import { Form, Button, Input, DatePicker, Tooltip, Select, TimePicker } from "antd";
+import { InfoCircleOutlined, QuestionCircleOutlined } from '@ant-design/icons';
 import TimezonePicker from 'react-bootstrap-timezone-picker';
 import 'react-bootstrap-timezone-picker/dist/react-bootstrap-timezone-picker.min.css';
 
+
+
 const { RangePicker } = DatePicker;
+var daysOfTheWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']; 
+const { Option } = Select;
+
+function handleChange(value) {
+    console.log(`selected ${value}`);
+}
 
 class MentorshipForm extends Component {
     render() {
@@ -16,37 +24,93 @@ class MentorshipForm extends Component {
                     name="testInput"
                     rules={[{ required: true, message: "Please input something" }]}
                 >
-                   <Input 
-                    placeholder = "Placeholder for test"
-                    prefix={
-                        <Tooltip title="This field will likely be deleted. I just wanted to test out this feature.">
-                          <InfoCircleOutlined style={{ color: 'rgba(0,0,0,.45)' }} />
-                        </Tooltip>
-                      }/>
-                </Form.Item>
-                <Form.Item 
-                    label = "Timeframe Seeking Mentorship"
-                    name = "selectedStartDate"
-                    rules={[{ required: true, message: "Please pick an estimated duration for how long you would like to use the platform" }]}
-                    >
-                       <RangePicker 
-                       picker = "month" 
-                       />
+                    <Input
+                        placeholder="Placeholder for test"
+                        prefix={
+                            <Tooltip title="This field will likely be deleted. I just wanted to test out this feature.">
+                                <InfoCircleOutlined style={{ color: 'rgba(0,0,0,.45)' }} />
+                            </Tooltip>
+                        } />
                 </Form.Item>
                 <Form.Item
-                label = "Please Select Your Time-zone"
-                name = "timeZone"
-                rules={[{ required: true, message: "This field is required" }]}
+                    label={
+                        <span>
+                            Timeframe Seeking Mentorship&nbsp;
+                        <Tooltip title="Please pick an estimated duration for how long you would like to use the platform">
+                                <QuestionCircleOutlined />
+                            </Tooltip>
+                        </span>
+                    }
+                    name="selectedStartDate"
+                    rules={[{ required: true, message: "This field is required" }]}
                 >
-                    <TimezonePicker
-                    absolute = {false}
-                    defaultValue = "America/Los_Angeles"
-                    placeholder = "Select timezone..."
-                    onChange = {this.handleChange}
+                    <RangePicker
+                        picker="month"
                     />
                 </Form.Item>
+                <Form.Item
+                    label="Please Select Your Time-zone"
+                    name="timeZone"
+                    rules={[{ required: true, message: "This field is required" }]}
+                >
+                    <TimezonePicker
+                        absolute={false}
+                        defaultValue="America/Los_Angeles"
+                        placeholder="Select timezone..."
+                        onChange={this.handleChange}
+                    />
+                </Form.Item>
+                <p> Please enter your reoccuring availability</p>
+                <Form.Item
+                    label="Monday"
+                    name="availabilityMonday"
+                >
+                    <Select
+                        mode="multiple" //so that multiple options can be selected 
+                        style={{ width: '100%' }}
+                        placeholder="select available time"
+                        onChange={handleChange}
+                        optionLabelProp="label"
+                    >
+                        <Option value="9-10AM" label="9-10AM">
+                        </Option>
+                        <Option value="10-11AM" label="10-11AM">
+                        </Option>
+                        <Option value="11AM-12PM" label="11AM-12AM">
+                        </Option>
+                        <Option value="12-1PM" label="12-1AM">
+                        </Option>
+                        <Option value="1-2PM" label="9-10AM">
+                        </Option>
+                    </Select>
+                   
+                </Form.Item>
+                <Form.Item
+                    label="Tuesday"
+                    name="availabilityTuesday"
+                >
+                    <Select
+                        mode="multiple"
+                        style={{ width: '100%' }}
+                        placeholder="select available time"
+                        onChange={handleChange}
+                        optionLabelProp="label"
+                    >
+                        <Option value="9-10AM" label="9-10AM">
+                        </Option>
+                        <Option value="10-11AM" label="10-11AM">
+                        </Option>
+                        <Option value="11AM-12PM" label="11AM-12AM">
+                        </Option>
+                        <Option value="12-1PM" label="12-1AM">
+                        </Option>
+                        <Option value="1-2PM" label="9-10AM">
+                        </Option>
+                    </Select>
+                   
+                </Form.Item>
                 <Button type="primary" htmlType="submit">
-                    Submit
+                        Submit
         </Button>
             </Form>
         );
