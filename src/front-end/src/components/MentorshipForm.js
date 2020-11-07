d66dab2934c94754fd7034cc6d279160ef545317
Katrina Martel <k.martel@outlook.com>
2020-11-07 09:33:33 -0800

    embedded a Google Calendar into the form

--- src/front-end/src/components/MentorshipForm.js
+++ src/front-end/src/components/MentorshipForm.js
@@ -9,6 +9,10 @@ const { RangePicker } = DatePicker;
 
 const { Option } = Select;
 
+const calendarStyle = {
+  border: 'solid 1px #777',
+};
+
 class MentorshipForm extends Component {
   render() {
     return (
@@ -55,7 +59,7 @@ class MentorshipForm extends Component {
           <Select
             // only one option can be selected:
             mode="single"
-            style={{ width: '100%' }}
+            style={calendarStyle}
             placeholder="select available time"
             optionLabelProp="label"
           >
@@ -92,6 +96,15 @@ class MentorshipForm extends Component {
           />
         </Form.Item>
         <p> Please enter your reoccuring availability:</p>
+        <iframe
+          title="avail"
+          src="https://calendar.google.com/calendar/embed?height=300&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=America%2FVancouver&amp;src=a2F0cmluYS5qb2xpLm1hcnRlbEBnbWFpbC5jb20&amp;src=ZGZ1a24wN2M4M21zaGRqNGg5NzhjZ2l0aWtAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;src=YWRkcmVzc2Jvb2sjY29udGFjdHNAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&amp;src=Z29vZ2xlLmNvbV9jZ2wwNTdjNDFhYmRwMXQ5aGFlaWJmaTg2Y0Bncm91cC5jYWxlbmRhci5nb29nbGUuY29t&amp;src=ZW4uY2FuYWRpYW4jaG9saWRheUBncm91cC52LmNhbGVuZGFyLmdvb2dsZS5jb20&amp;color=%23039BE5&amp;color=%23D81B60&amp;color=%2333B679&amp;color=%234285F4&amp;color=%230B8043&amp;showTitle=0&amp;showPrint=0"
+          style={{ border: 'solid 1px #777' }}
+          width="400"
+          height="300"
+          frameBorder="0"
+          scrolling="no"
+        />
         <Form.Item label="Monday" name="availabilityMonday">
           <Select
             // so that multiple options can be selected:
