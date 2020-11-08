9976a2b914a40b44c13df12fa93960fb78ca576e
Jean-Philippe Abadir <jpabadir@gmail.com>
2020-11-08 20:50:18 +0100

    Implement signup

--- src/front-end/src/components/MentorshipForm.js
+++ src/front-end/src/components/MentorshipForm.js
@@ -5,12 +5,33 @@ import { InfoCircleOutlined, QuestionCircleOutlined } from '@ant-design/icons';
 import TimezonePicker from 'react-bootstrap-timezone-picker';
 import 'react-bootstrap-timezone-picker/dist/react-bootstrap-timezone-picker.min.css';
 
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyAdutyXDo_0GvanZ58V1l702Co_zQu4a3M',
+  authDomain: 'mentormatch-afa46.firebaseapp.com',
+  databaseURL: 'https://mentormatch-afa46.firebaseio.com',
+  projectId: 'mentormatch-afa46',
+  storageBucket: 'mentormatch-afa46.appspot.com',
+  messagingSenderId: '515592331593',
+  appId: '1:515592331593:web:c38e0d3defadf9886cfa98',
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
 const { RangePicker } = DatePicker;
 
 const { Option } = Select;
 
 const onFinish = (values) => {
-  console.log('Success:', values);
+  firebase
+    .auth()
+    .createUserWithEmailAndPassword(values.emailInput, values.password)
+    .catch((error) => {
+      console.log(error);
+    });
 };
 
 const onFinishFailed = (values) => {
