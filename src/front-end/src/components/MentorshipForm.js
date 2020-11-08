353063a5951eb027f66e2f2bb0eff1ae785ec07b
Katrina Martel <k.martel@outlook.com>
2020-11-08 15:16:14 -0800

    added setup for responsive layout

--- src/front-end/src/components/MentorshipForm.js
+++ src/front-end/src/components/MentorshipForm.js
@@ -94,37 +94,7 @@ class MentorshipForm extends Component {
               onChange={this.handleChange}
             />
           </Form.Item>
-          <p> Please enter your reoccuring availability:</p>
-          <Form.Item label="Monday" name="availabilityMonday">
-            <Select
-              // so that multiple options can be selected:
-              mode="multiple"
-              style={{ width: '100%' }}
-              placeholder="select available time"
-              optionLabelProp="label"
-            >
-              <Option value="9-10AM" label="9-10AM" />
-              <Option value="10-11AM" label="10-11AM" />
-              <Option value="11AM-12PM" label="11AM-12AM" />
-              <Option value="12-1PM" label="12-1PM" />
-              <Option value="1-2PM" label="1-2PM" />
-            </Select>
-          </Form.Item>
-          <Form.Item label="Tuesday" name="availabilityTuesday">
-            <Select
-              // so that multiple options can be selected:
-              mode="multiple"
-              style={{ width: '100%' }}
-              placeholder="select available time"
-              optionLabelProp="label"
-            >
-              <Option value="9-10AM" label="9-10AM" />
-              <Option value="10-11AM" label="10-11AM" />
-              <Option value="11AM-12PM" label="11AM-12PM" />
-              <Option value="12-1PM" label="12-1PM" />
-              <Option value="1-2PM" label="1-2PM" />
-            </Select>
-          </Form.Item>
+          <p> Please enter your availability:</p>
           <Button type="primary" htmlType="submit">
             Submit
           </Button>
