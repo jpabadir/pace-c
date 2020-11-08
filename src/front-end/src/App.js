0be02fac1ffec0e723be1331d25fa05b4ff90faf
Jean-Philippe Abadir <jpabadir@gmail.com>
2020-11-08 13:48:26 +0100

    Use React Router to split page between login and signup form

--- src/front-end/src/App.js
+++ src/front-end/src/App.js
@@ -1,20 +1,36 @@
 import React from 'react';
-import { Button } from 'antd';
+import { Layout, Menu } from 'antd';
 import './App.css';
+import { NavLink, Switch, Route } from 'react-router-dom';
 import MentorshipForm from './components/MentorshipForm';
+import Login from './components/Login';
 
-function callServer() {
-  // const request = require('request');
-  // request('http://localhost:8000/', (error, response, body) => {});
-}
+const { Header, Content, Footer } = Layout;
 
 function App() {
   return (
     <div className="App">
-      <Button type="primary" onClick={callServer}>
-        Call the server!
-      </Button>
-      <MentorshipForm />
+      <Layout className="layout">
+        <Header>
+          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+            <Menu.Item key="1">
+              <NavLink to="/signup-form">Signup Form</NavLink>
+            </Menu.Item>
+            <Menu.Item key="2">
+              <NavLink to="/login">Login</NavLink>
+            </Menu.Item>
+          </Menu>
+        </Header>
+        <Content style={{ padding: '0 50px' }}>
+          <Switch>
+            <Route exact path="/signup-form" component={MentorshipForm} />
+            <Route exact path="/login" component={Login} />
+          </Switch>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>
+          MentorMatch ©2020 Created by UBCO students
+        </Footer>
+      </Layout>
     </div>
   );
 }
