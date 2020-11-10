2c126ea4ff65579c8ecddcf245d4f9b4659b5a72
Jean-Philippe Abadir <jpabadir@gmail.com>
2020-11-10 01:36:38 +0100

    Revert "Merge branch 'develop' into 188-style-static-form"
    This reverts commit b44af18c719d13b622cfd9c1ad13d677c05653d4, reversing
    changes made to 353063a5951eb027f66e2f2bb0eff1ae785ec07b.
    
    Merge

--- src/front-end/src/components/MentorshipForm.js
+++ src/front-end/src/components/MentorshipForm.js
@@ -4,77 +4,63 @@ import { Form, Button, Input, DatePicker, Tooltip, Select } from 'antd';
 import { InfoCircleOutlined, QuestionCircleOutlined } from '@ant-design/icons';
 import TimezonePicker from 'react-bootstrap-timezone-picker';
 import 'react-bootstrap-timezone-picker/dist/react-bootstrap-timezone-picker.min.css';
-import firebase from '../firebase-init';
 
 const { RangePicker } = DatePicker;
 
 const { Option } = Select;
 
-const onFinish = (values) => {
-  firebase
-    .auth()
-    .createUserWithEmailAndPassword(values.emailInput, values.password)
-    .catch((error) => {
-      console.log(error);
-    });
-};
-
-const onFinishFailed = (values) => {
-  console.log('Failed submit:', values);
-};
-
 class MentorshipForm extends Component {
   render() {
     return (
-     // CSS class names follow kebab-case naming conventions
-     <div className="mentorship-form">
-      <Form onFinish={onFinish} onFinishFailed={onFinishFailed}>
-        <Form.Item
-          // TEST ITEM (will be deleted):
-          label="Test"
-          name="testInput"
-          // must have an input:
-          rules={[{ required: true, message: 'Please input something' }]}
-        >
-          <Input
-            placeholder="Placeholder for test"
-            prefix={
-              // tooltip test:
-              <Tooltip title="This field will likely be deleted. I just wanted to test out this feature.">
-                <InfoCircleOutlined style={{ color: 'rgba(0,0,0,.45)' }} />
-              </Tooltip>
-            }
-          />
-        </Form.Item>
-        <Form.Item
-          label="Name"
-          name="nameInput"
-          // must have an input:
-          rules={[{ required: true, message: 'Please input something' }]}
-        >
-          <Input placeholder="please enter your name" />
-        </Form.Item>
-        <Form.Item
-          label="Email"
-          name="emailInput"
-          // must have an input:
-          rules={[{ required: true, message: 'Please input something' }]}
-        >
-          <Input placeholder="please enter your email" />
-        </Form.Item>
-        <Form.Item
-          label="Are you a mentor or mentee?"
-          name="mentorMentee"
-          // must have an input:
-          rules={[{ required: true, message: 'Please input something' }]}
-        >
-          <Select
-            // only one option can be selected:
-            mode="single"
-            style={{ width: '100%' }}
-            placeholder="select available time"
-            optionLabelProp="label"
+      // CSS class names follow kebab-case naming conventions
+      <div className="mentorship-form">
+        <Form>
+          <Form.Item
+            // TEST ITEM (will be deleted):
+            label="Test"
+            name="testInput"
+            // must have an input:
+            rules={[{ required: true, message: 'Please input something' }]}
           >
+            <Input
+              placeholder="Placeholder for test"
+              prefix={
+                // tooltip test:
+                <Tooltip title="This field will likely be deleted. I just wanted to test out this feature.">
+                  <InfoCircleOutlined style={{ color: 'rgba(0,0,0,.45)' }} />
+                </Tooltip>
+              }
+            />
+          </Form.Item>
+          <Form.Item
+            label="Name"
+            name="nameInput"
+            // must have an input:
+            rules={[{ required: true, message: 'Please input something' }]}
+          >
+            <Input placeholder="please enter your name" />
+          </Form.Item>
+          <Form.Item
+            label="Email"
+            name="emailInput"
+            // must have an input:
+            rules={[{ required: true, message: 'Please input something' }]}
+          >
+            <Input placeholder="please enter your email" />
+          </Form.Item>
+          <Form.Item
+            label="Are you a mentor or mentee?"
+            name="mentorMentee"
+            // must have an input:
+            rules={[{ required: true, message: 'Please input something' }]}
+          >
+            <Select
+              // only one option can be selected:
+              mode="single"
+              style={{ width: '100%' }}
+              placeholder="select available time"
+              optionLabelProp="label"
+            >
               <Option value="Mentor" label="Mentor" />
               <Option value="Mentee" label="Mentee" />
             </Select>
@@ -109,26 +95,18 @@ class MentorshipForm extends Component {
             />
           </Form.Item>
           <p> Please enter your availability:</p>
-        <Form.Item
-          label="Password"
-          name="password"
-          // must have an input:
-          rules={[{ required: true, message: 'Please input something' }]}
-        >
-          <Input.Password placeholder="password" />
-        </Form.Item>
-        <Button type="primary" htmlType="submit">
-          Submit
-        </Button>
-        <p>{/* used to space buttons */}</p>
-        <Button type="primary" htmlType="button">
-          Back
-        </Button>
-        <Button type="primary" htmlType="button">
-          Next
-        </Button>
-      </Form>
-    </div>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+          <p>{/* used to space buttons */}</p>
+          <Button className="back-button" type="primary" htmlType="button">
+            Back
+          </Button>
+          <Button className="next-button" type="primary" htmlType="button">
+            Next
+          </Button>
+        </Form>
+      </div>
     );
   }
 }
