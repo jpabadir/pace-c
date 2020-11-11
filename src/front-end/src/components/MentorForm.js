911f8ec0395f04a3fafc48234d535748b7a32e66
wrc9817 <71383839+wrc9817@users.noreply.github.com>
2020-11-11 09:59:14 -0800

    add description area

--- src/front-end/src/components/MentorForm.js
+++ src/front-end/src/components/MentorForm.js
@@ -5,6 +5,7 @@ import { Form, Button, Input, DatePicker, Tooltip, Select } from 'antd';
 import { InfoCircleOutlined, QuestionCircleOutlined } from '@ant-design/icons';
 import TimezonePicker from 'react-bootstrap-timezone-picker';
 import 'react-bootstrap-timezone-picker/dist/react-bootstrap-timezone-picker.min.css';
+import TextArea from 'antd/lib/input/TextArea';
 
 const { RangePicker } = DatePicker;
 
@@ -15,6 +16,9 @@ class MentorForm extends Component {
     return (
     <div className="MentorForm"> 
       <Form>
+      <h1>
+        Mentor is .......
+      </h1>
         <Form.Item
           // TEST ITEM (will be deleted):
           label="Organization"
@@ -23,7 +27,7 @@ class MentorForm extends Component {
           rules={[{ required: true, message: 'Please input something' }]}
         >
           <Input
-            placeholder="Please type the organization that you're planning to be a Mentor with"
+            placeholder="Please enter the organization that you're planning to be a Mentor with"
             prefix={
               // tooltip test:
               <Tooltip title="Organization">
@@ -61,6 +65,21 @@ class MentorForm extends Component {
             defaultValue="America/Los_Angeles"
             placeholder="Select timezone..."
             onChange={this.handleChange}
+          />
+        </Form.Item>
+        <Form.Item
+          label="Please enter your password"
+          name="Password"
+          rules={[{ required: true, message: 'Please input something' }]}
+        >
+          <Input
+            placeholder="Please enter your password"
+            prefix={
+              // tooltip test:
+              <Tooltip title="Password should be at least 8 characters.">
+                <InfoCircleOutlined style={{ color: 'rgba(0,0,0,.45)' }} />
+              </Tooltip>
+            }
           />
         </Form.Item>
         <Button type="primary" htmlType="submit">
