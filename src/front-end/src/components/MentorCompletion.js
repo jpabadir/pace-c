b3e421713e65f58cb2c0f4f38387b31297ee7c01
wrc9817 <71383839+wrc9817@users.noreply.github.com>
2020-11-13 15:29:37 +0800

    Add mentee completion and mentor completion pages

---
+++ src/front-end/src/components/MentorCompletion.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default () => (
+  <div className="MentorCompletion">
+    <p>
+      Congradulations! You have successfully created your own account. Please
+      activate your account through the email we just sent and click
+      <a
+        className="loginlink"
+        href="http://localhost:3000/login
+      "
+      >
+        here
+      </a>
+      to login!
+    </p>
+  </div>
+);
