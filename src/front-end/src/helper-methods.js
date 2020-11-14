21aa43adeac8d3359d29598b143b247b9c4adf13
Jean-Philippe Abadir <jpabadir@gmail.com>
2020-11-14 23:32:34 +0100

    Save all fields from mentor singup into DO
    Save all fields from mentor form into DB

--- src/front-end/src/helper-methods.js
+++ src/front-end/src/helper-methods.js
@@ -1,24 +1,41 @@
 import firebase from './firebase-init';
 
-export function createUserInFirebase(email, password) {
-  firebase
-    .auth()
-    .createUserWithEmailAndPassword(email, password)
-    .then((userCredential) => {
-      userCredential.user
-        .sendEmailVerification()
-        .then(() => {
-          console.log("Email verification sent to user's email");
-        })
-        .catch((error) => {
-          console.log(error);
-        });
+function mySendEmailVerification(user) {
+  user
+    .sendEmailVerification()
+    .then(() => {
+      console.log("Email verification sent to user's email");
     })
     .catch((error) => {
       console.log(error);
     });
 }
 
-export function saveToDB(reference, objectToSave) {
-  firebase.database().ref(reference).push().set(objectToSave);
+export function createUserInFirebase(email, password) {
+  return new Promise((resolve) => {
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then((userCredential) => {
+        mySendEmailVerification(userCredential.user);
+        resolve(userCredential.user);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  });
+}
+
+export function setInDB(reference, child, objectToSave) {
+  firebase.database().ref(reference).child(child).set(objectToSave);
+}
+
+export function marshallMentorInfo(mentorFormValues) {
+  return {
+    organization: mentorFormValues.organization,
+    email: mentorFormValues.emailInput,
+    name: mentorFormValues.nameInput,
+    timeZone: mentorFormValues.timeZone,
+    userType: 'mentor',
+  };
 }
