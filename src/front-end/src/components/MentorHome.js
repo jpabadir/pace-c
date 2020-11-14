35e733f6821fbf05e2e44952b3b4c8259ccddd4b
Katrina Martel <k.martel@outlook.com>
2020-11-13 19:19:08 -0800

    added comments to MentorHome

--- src/front-end/src/components/MentorHome.js
+++ src/front-end/src/components/MentorHome.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
 import { Form, Button, Select } from 'antd';
-// eslint-disable-next-line import/no-extraneous-dependencies
 import 'react-bootstrap-timezone-picker/dist/react-bootstrap-timezone-picker.min.css';
 import { UserOutlined, CheckOutlined, CloseOutlined } from '@ant-design/icons';
 
 const { Option } = Select;
 
+// NOTE: Fields surrounded by square brackets []
+// denote fields that are required to be filled-in from the database
+
 // eslint-disable-next-line react/prefer-stateless-function
 class MentorHome extends Component {
   render() {
     return (
+      // What looks like an empty tag (<>) is known as a Fragment
+      // Fragments are needed to turn HTML into JSX
       <>
         <Button type="primary" htmlType="button" id="logout">
           Logout
@@ -18,6 +22,7 @@ class MentorHome extends Component {
           <h1>
             <center>Welcome back, [Mentor Name from DB]</center>
           </h1>
+
           <Form.Item label="Your current mentees" name="userMentees">
             <Select
               // so that single options can be selected:
@@ -33,9 +38,7 @@ class MentorHome extends Component {
             </Select>
           </Form.Item>
         </Form>
-        {/* the form below should change the information depeding on: */}
-        {/* the mentee selected */}
-        {/* button below should link to an appropriate page, once logged out */}
+        {/* replace what's in square brackets [] w/ info from DB */}
         <div className="mentee-requests">
           <h2> Pending Mentee Requests</h2>
           <p>
