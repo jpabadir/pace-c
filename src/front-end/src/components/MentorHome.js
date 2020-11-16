255ae0c9fa4801b2ae6b854dc3719b4a1ee31914
wrc9817 <71383839+wrc9817@users.noreply.github.com>
2020-11-16 04:47:13 -0800

    Update MentorHome.js

--- src/front-end/src/components/MentorHome.js
+++ src/front-end/src/components/MentorHome.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import { Form, Button, Select } from 'antd';
-// eslint-disable-next-line import/no-extraneous-dependencies
 import 'react-bootstrap-timezone-picker/dist/react-bootstrap-timezone-picker.min.css';
+import { UserOutlined, CheckOutlined, CloseOutlined } from '@ant-design/icons';
 import fire from '../firebase-init';
 
 const { Option } = Select;
+
+// NOTE: Fields surrounded by square brackets []
+// denote fields that are required to be filled-in from the database
 
 // eslint-disable-next-line react/prefer-stateless-function
 class MentorHome extends Component {
@@ -19,41 +22,76 @@ class MentorHome extends Component {
 
   render() {
     return (
-      <Form>
-        <h1>
-          <center>Welcome back, Mentor!</center>
-        </h1>
-        <Form.Item label="Your current mentees" name="userMentees">
-          <Select
-            // so that single options can be selected:
-            mode="single"
-            style={{ width: '100%' }}
-            placeholder="select a mentee"
-            optionLabelProp="label"
-          >
-            {/* the amount of options should be the amount of mentees */}
-            <Option value="Mentee1" label="Mentee1" />
-            <Option value="Mentee2" label="Mentee2" />
-            <Option value="Mentee3" label="Mentee3" />
-          </Select>
-        </Form.Item>
-        <h2> Information about &quot;insert selected mentee name&quot;</h2>
-        {/* the form below should change the information depeding on: */}
-        {/* the mentee selected */}
-        <p>
-          Information 1: &quot;insert information1 about selected mentee&quot;
-        </p>
-        <p>
-          Information 2: &quot;insert information2 about selected mentee&quot;
-        </p>
-        <p>
-          Information 3: &quot;insert information3 about selected mentee&quot;
-        </p>
-        {/* button below should link to an appropriate page, once logged out */}
-        <Button onClick={this.onLogout} type="primary" htmlType="button">
+      // What looks like an empty tag (<>) is known as a Fragment
+      // Fragments are needed to turn HTML into JSX
+      <>
+        <Button type="primary" htmlType="button" id="logout">
           Logout
         </Button>
-      </Form>
+        <Form>
+          <h1>
+            <center>Welcome back, [Mentor Name from DB]</center>
+          </h1>
+
+          <Form.Item label="Your current mentees" name="userMentees">
+            <Select
+              // so that single options can be selected:
+              mode="single"
+              style={{ width: '100%' }}
+              placeholder="select a mentee"
+              optionLabelProp="label"
+            >
+              {/* the amount of options should be the amount of mentees */}
+              <Option value="Mentee1" label="Mentee1" />
+              <Option value="Mentee2" label="Mentee2" />
+              <Option value="Mentee3" label="Mentee3" />
+            </Select>
+          </Form.Item>
+        </Form>
+        {/* replace what's in square brackets [] w/ info from DB */}
+        <div className="mentee-requests">
+          <h2> Pending Mentee Requests</h2>
+          <p>
+            Click <CheckOutlined className="check" /> to accept a Mentee&apos;s
+            request or <CloseOutlined className="close" /> to decline.
+          </p>
+          <div className="individual-mentee">
+            <table>
+              <tr>
+                <th>
+                  <UserOutlined className="user-icon" />
+                  [Name from DB]
+                  <Button type="text" onClick={this.handleButtonClick}>
+                    <CheckOutlined className="check" id="accept" />{' '}
+                  </Button>
+                  <Button type="text" onClick={this.handleButtonClick}>
+                    <CloseOutlined className="close" id="decline" />{' '}
+                  </Button>
+                </th>
+              </tr>
+              <tr>
+                <td>
+                  <strong> Email: </strong> [Email from Database]
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <strong> Skills Seeking Mentorship: </strong> [Skills Array
+                  From Database]
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <strong> Description: </strong> [Description From Database]{' '}
+                </td>
+              </tr>
+            </table>
+          </div>
+          <Button onClick={this.onLogout} type="primary" htmlType="button">
+            Logout
+          </Button>
+        </div>
+      </>
     );
   }
 }
