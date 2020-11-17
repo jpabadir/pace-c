95ce02dc75ac2d16badef4d94f6bb9e7c88471ce
Aleks K <aleksschoool41@gmail.com>
2020-11-17 02:21:59 -0800

    fixed login

--- src/front-end/src/components/Login.js
+++ src/front-end/src/components/Login.js
@@ -46,58 +46,59 @@ class Login extends Component {
 
   render() {
     return (
-      <Form>
-        <p>Welcome to MentorMatch!</p>
-        <p>Login to access the mentorship portal</p>
-        <Form.Item
-          type="email"
-          label="Email"
-          name="email"
-          // must have an input:
-          rules={[{ required: true, message: 'Please input something' }]}
-        >
-          <Input
+      <div className="Login">
+        <Form>
+          <h1>Login to access the mentorship portal</h1>
+          <Form.Item
             type="email"
-            placeholder="please enter your email"
-            id="useremail"
-            onInput={this.onChange}
-            value={this.state.email}
-            onChange={this.handleEmailChange}
-          />
-        </Form.Item>
-        <Form.Item
-          type="password"
-          label="Password"
-          name="password"
-          // must have an input:
-          rules={[{ required: true, message: 'Please input something' }]}
-        >
-          <Input
+            label="Email"
+            name="email"
+            // must have an input:
+            rules={[{ required: true, message: 'Please input something' }]}
+          >
+            <Input
+              type="email"
+              placeholder="please enter your email"
+              id="useremail"
+              onInput={this.onChange}
+              value={this.state.email}
+              onChange={this.handleEmailChange}
+            />
+          </Form.Item>
+          <Form.Item
             type="password"
-            placeholder="please enter your password"
-            id="userpassword"
-            onInput={this.onChange}
-          />
-        </Form.Item>
-        <p>{/* used to space buttons */}</p>
-        {/* button below should send the information from the page: */}
-        {/* login to the database. To verify the account password/email */}
-        <Button type="primary" htmlType="submit" onClick={this.onLogin}>
-          Login
-        </Button>
-        {/* link below should allow the user to reset password: */}
-        <p>
-          Forgot your password? Enter your email above and click&nbsp;
-          <Button onClick={this.localResetPassword}>here!</Button>
-        </p>
-        {/* below is used as a space between the two links */}
-        &nbsp;
-        <p>Don&apos;t have an account yet?</p>
-        {/* button below should link to the createaccount page: */}
-        <Button type="primary" htmlType="button">
-          Create Account
-        </Button>
-      </Form>
+            label="Password"
+            name="password"
+            // must have an input:
+            rules={[{ required: true, message: 'Please input something' }]}
+          >
+            <Input
+              type="password"
+              placeholder="please enter your password"
+              id="userpassword"
+              onInput={this.onChange}
+            />
+          </Form.Item>
+          <p>{/* used to space buttons */}</p>
+          {/* button below should send the information from the page: */}
+          {/* login to the database. To verify the account password/email */}
+          <Button type="primary" htmlType="submit" onClick={this.onLogin}>
+            Login
+          </Button>
+          {/* link below should allow the user to reset password: */}
+          <p>
+            Forgot your password? Enter your email above and click&nbsp;
+            <Button onClick={this.localResetPassword}>here!</Button>
+          </p>
+          {/* below is used as a space between the two links */}
+          &nbsp;
+          <p>Don&apos;t have an account yet?</p>
+          {/* button below should link to the createaccount page: */}
+          <Button type="primary" htmlType="button">
+            Create Account
+          </Button>
+        </Form>
+      </div>
     );
   }
 }
