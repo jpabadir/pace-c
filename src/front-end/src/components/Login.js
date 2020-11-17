0516a7d4ecd16a203ef2d2eac564147cbd392822
Jean-Philippe Abadir <jpabadir@gmail.com>
2020-11-17 10:24:26 +0100

    Revert "Merge pull request #274 from jpabadir/265-Style-login-page"
    This reverts commit 8cb77a9ddb24730c32483bfde6c48b8aeab17783, reversing
    changes made to c2be6dce48e27bada79610a9afbaca61af5edecd.
    
    Revert merge because broken functionality

--- src/front-end/src/components/Login.js
+++ src/front-end/src/components/Login.js
@@ -46,50 +46,58 @@ class Login extends Component {
 
   render() {
     return (
-      <div className="Login">
-        <Form>
-          <h1>Login to access the mentorship portal</h1>
-          <Form.Item
+      <Form>
+        <p>Welcome to MentorMatch!</p>
+        <p>Login to access the mentorship portal</p>
+        <Form.Item
+          type="email"
+          label="Email"
+          name="email"
+          // must have an input:
+          rules={[{ required: true, message: 'Please input something' }]}
+        >
+          <Input
             type="email"
-            label="Email"
-            name="email"
-            // must have an input:
-            rules={[{ required: true, message: 'Please input something' }]}
-          >
-            <Input
-              placeholder="please enter your email"
-              value={this.state.email}
-              onChange={this.handleEmailChange}
-            />
-          </Form.Item>
-          <Form.Item
-            label="Password"
-            name="password"
-            // must have an input:
-            rules={[{ required: true, message: 'Please input something' }]}
-          >
-            <Input placeholder="please enter your password" type="password" />
-          </Form.Item>
-          <p>{/* used to space buttons */}</p>
-          {/* button below should send the information from the page: */}
-          {/* login to the database. To verify the account password/email */}
-          <Button type="primary" htmlType="submit">
-            Login
-          </Button>
-          {/* link below should allow the user to reset password: */}
-          <p>
-            Forgot your password? Enter your email above and click&nbsp;
-            <Button onClick={this.localResetPassword}>here!</Button>
-          </p>
-          {/* below is used as a space between the two links */}
-          &nbsp;
-          <p>Don&apos;t have an account yet?</p>
-          {/* button below should link to the createaccount page:  */}
-          <Button type="primary" htmlType="button">
-            Create Account
-          </Button>
-        </Form>
-      </div>
+            placeholder="please enter your email"
+            id="useremail"
+            onInput={this.onChange}
+            value={this.state.email}
+            onChange={this.handleEmailChange}
+          />
+        </Form.Item>
+        <Form.Item
+          type="password"
+          label="Password"
+          name="password"
+          // must have an input:
+          rules={[{ required: true, message: 'Please input something' }]}
+        >
+          <Input
+            type="password"
+            placeholder="please enter your password"
+            id="userpassword"
+            onInput={this.onChange}
+          />
+        </Form.Item>
+        <p>{/* used to space buttons */}</p>
+        {/* button below should send the information from the page: */}
+        {/* login to the database. To verify the account password/email */}
+        <Button type="primary" htmlType="submit" onClick={this.onLogin}>
+          Login
+        </Button>
+        {/* link below should allow the user to reset password: */}
+        <p>
+          Forgot your password? Enter your email above and click&nbsp;
+          <Button onClick={this.localResetPassword}>here!</Button>
+        </p>
+        {/* below is used as a space between the two links */}
+        &nbsp;
+        <p>Don&apos;t have an account yet?</p>
+        {/* button below should link to the createaccount page: */}
+        <Button type="primary" htmlType="button">
+          Create Account
+        </Button>
+      </Form>
     );
   }
 }
