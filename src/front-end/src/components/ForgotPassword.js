acf89c6d86e1f14c6c5b03798464f1437e0a5506
wrc9817 <71383839+wrc9817@users.noreply.github.com>
2020-11-19 01:14:19 +0800

    add forgot password page

---
+++ src/front-end/src/components/ForgotPassword.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import { Form, Button, Input } from 'antd';
+import { resetPassword } from '../helper-methods';
+
+class ForgotPassword extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+    };
+    this.localResetPassword = this.localResetPassword.bind(this);
+    this.handleEmailChange = this.handleEmailChange.bind(this);
+  }
+
+  localResetPassword() {
+    // TODO: implement some validation here
+    resetPassword(this.state.email);
+  }
+
+  handleEmailChange(event) {
+    this.setState({ email: event.target.value });
+  }
+
+  render() {
+    return (
+      <div className="ForgotPassword">
+        <Form>
+          <h1>Reset your password</h1>
+          <Form.Item
+            label="Email"
+            name="emailInput"
+            // must have an input:
+            rules={[
+              {
+                required: true,
+                message: 'Please enter your email to reset your password',
+              },
+            ]}
+          >
+            <Input
+              placeholder="name@example.com"
+              onChange={this.handleEmailChange}
+            />
+          </Form.Item>
+          <Button
+            onClick={this.localResetPassword}
+            type="primary"
+            htmlType="submit"
+          >
+            Confirm
+          </Button>
+        </Form>
+      </div>
+    );
+  }
+}
+// exports:
+export default ForgotPassword;
