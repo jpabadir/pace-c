39471a6ef9ef01651e57e4c29807b60d0f598fb4
Jean-Philippe Abadir <jpabadir@gmail.com>
2020-11-20 15:09:56 +0100

    Implement mentee decline

--- src/front-end/src/components/MentorSuggested.js
+++ src/front-end/src/components/MentorSuggested.js
@@ -30,7 +30,7 @@ class MentorSuggested extends Component {
   acceptMentee(menteeUid) {
     const userRef = fire.database().ref('users/' + this.state.loggedUid);
     userRef.once('value', (snapshot) => {
-      // Build updated accepted and suggested mentee arrays
+      // Build updated declined and suggested mentees arrays
       const acceptedMentees = snapshot.val().acceptedMentees
         ? snapshot.val().acceptedMentees
         : [];
@@ -57,7 +57,32 @@ class MentorSuggested extends Component {
   }
 
   declineMentee(menteeUid) {
-    alert(menteeUid);
+    const userRef = fire.database().ref('users/' + this.state.loggedUid);
+    userRef.once('value', (snapshot) => {
+      // Build updated declined and suggested mentees arrays
+      const declinedMentees = snapshot.val().declinedMentees
+        ? snapshot.val().declinedMentees
+        : [];
+      declinedMentees.push(menteeUid);
+      const suggestedMentees = snapshot.val().suggestedMentees;
+      suggestedMentees.splice(suggestedMentees.indexOf(menteeUid), 1);
+
+      // Update the accepted and suggested mentees in DB
+      userRef.child('declinedMentees').set(declinedMentees);
+      userRef.child('suggestedMentees').set(suggestedMentees);
+
+      // Update the state to reflect the DB changes
+      const currentMentees = this.state.mentees;
+      const newMentees = currentMentees.filter((mentee) => {
+        return mentee.id !== menteeUid;
+      });
+      this.setState({
+        mentees: newMentees,
+      });
+
+      // Confirm with the user that this was succesful
+      alert('Mentee request declined.');
+    });
   }
 
   authListener() {
