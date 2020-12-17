6544b479a970c94af0bce76b5a376be52f550bf2
Katrina Martel <k.martel@outlook.com>
2020-12-16 18:49:40 -0800

    modification to footer

--- src/front-end/src/App.js
+++ src/front-end/src/App.js
@@ -52,7 +52,7 @@ function App() {
         <Footer
           style={{
             textAlign: 'center',
-            position: 'static',
+            position: 'fixed',
             bottom: 0,
             width: '100%',
             overflow: 'flex',
