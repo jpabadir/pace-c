61ff2bb22021f096e0934655429bb04d9b725c72
wrc9817 <71383839+wrc9817@users.noreply.github.com>
2021-01-11 18:40:20 +0800

    Update function OnFinish
    If email exists, alert. If not, create new account.

--- src/front-end/src/components/MentorForm.js
+++ src/front-end/src/components/MentorForm.js
@@ -5,6 +5,7 @@ import { InfoCircleOutlined } from '@ant-design/icons';
 import TimezonePicker from 'react-bootstrap-timezone-picker';
 import 'react-bootstrap-timezone-picker/dist/react-bootstrap-timezone-picker.min.css';
 import MentorCompletion from './MentorCompletion';
+import firebase from '../firebase-init';
 import {
   setInDB,
   createUserInFirebase,
@@ -34,14 +35,30 @@ class MentorForm extends Component {
 
   onFinish(values) {
     // Set submit to true
-    this.setState({ isSubmitted: true });
-
-    // Create user and store user's form info
-    createUserInFirebase(values.emailInput, values.password).then(
-      (createdUser) => {
-        setInDB('users', createdUser.uid, marshallMentorInfo(values));
-      },
-    );
+    firebase
+      .database()
+      .ref()
+      .child('users')
+      .orderByChild('email')
+      .equalTo(values.emailInput)
+      .once('value')
+      .then((snapshot) => {
+        // if exists, alert.
+        if (snapshot.exists()) {
+          // probably alerts in other form of message.
+          window.alert('mail already exists.');
+          this.setState({ isSubmitted: false });
+          // if not exists, create new account and jump to completion page.
+        } else {
+          console.log('not found');
+          createUserInFirebase(values.emailInput, values.password).then(
+            (createdUser) => {
+              setInDB('users', createdUser.uid, marshallMentorInfo(values));
+            },
+          );
+          this.setState({ isSubmitted: true });
+        }
+      });
   }
 
   onFinishFailed(values) {
