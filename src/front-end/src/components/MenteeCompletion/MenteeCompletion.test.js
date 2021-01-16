bb6dafc9014c0dabd6175423b7d53bdf0561562a
Katrina Martel <k.martel@outlook.com>
2021-01-15 22:28:18 -0800

    initial folder setup

---
+++ src/front-end/src/components/MenteeCompletion/MenteeCompletion.test.js
@@ -0,0 +1,9 @@
+// import React from 'react';
+// import { render, unmountComponentAtNode } from 'react-dom';
+// import { mockWindowMatchMedia } from '../../helper-methods';
+
+// import MenteeCompletion from './MenteeCompletion';
+
+// let container = null;
+
+// mockWindowMatchMedia();
---
+++ src/front-end/src/components/MenteeDisplay/MenteeDisplay.test.js
@@ -0,0 +1,9 @@
+// import React from 'react';
+// import { render, unmountComponentAtNode } from 'react-dom';
+// import { mockWindowMatchMedia } from '../../helper-methods';
+
+// import MenteeDisplay from './MenteeDisplay';
+
+// let container = null;
+
+// mockWindowMatchMedia();
