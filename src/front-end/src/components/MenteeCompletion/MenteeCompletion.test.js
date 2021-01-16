12fab2909ac02e4d187e839d4c208c395c0789bb
Katrina Martel <k.martel@outlook.com>
2021-01-16 14:55:34 -0800

    initial tests

--- src/front-end/src/components/MenteeCompletion/MenteeCompletion.test.js
+++ src/front-end/src/components/MenteeCompletion/MenteeCompletion.test.js
@@ -1,9 +1,28 @@
-// import React from 'react';
-// import { render, unmountComponentAtNode } from 'react-dom';
-// import { mockWindowMatchMedia } from '../../helper-methods';
+import React from 'react';
+import { render, unmountComponentAtNode } from 'react-dom';
+import { mockWindowMatchMedia } from '../../helper-methods';
 
-// import MenteeCompletion from './MenteeCompletion';
+import MenteeCompletion from './MenteeCompletion';
 
-// let container = null;
+let container = null;
 
-// mockWindowMatchMedia();
+mockWindowMatchMedia();
+
+beforeEach(() => {
+  container = document.createElement('div');
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  // cleanup on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+it('renders without crashing', () => {
+  render(<MenteeCompletion />, container);
+  expect(container.textContent).toBe(
+    "Congratulations! You have successfully submitted your request for mentorship. We'll send you an email once we match you with a mentor!",
+  );
+});
--- src/front-end/src/components/MenteeDisplay/MenteeDisplay.test.js
+++ src/front-end/src/components/MenteeDisplay/MenteeDisplay.test.js
@@ -1,9 +1,34 @@
-// import React from 'react';
-// import { render, unmountComponentAtNode } from 'react-dom';
-// import { mockWindowMatchMedia } from '../../helper-methods';
+import React from 'react';
+import { render, unmountComponentAtNode } from 'react-dom';
+import { mockWindowMatchMedia } from '../../helper-methods';
 
-// import MenteeDisplay from './MenteeDisplay';
+import MenteeDisplay from './MenteeDisplay';
 
-// let container = null;
+let container = null;
 
-// mockWindowMatchMedia();
+mockWindowMatchMedia();
+
+beforeEach(() => {
+  container = document.createElement('div');
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  // cleanup on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+test('render the body of the card correctly', () => {
+  render(<MenteeDisplay />, container);
+  const p = document.getElementsByTagName('p');
+  let i = 0;
+  while (i < p.length) {
+    expect(container.contains(p[i])).toBe(true);
+    i += 1;
+  }
+  expect(p[0].textContent).toBe(' Email: ');
+  expect(p[1].textContent).toBe(' Skills Seeking Mentorship: ');
+  expect(p[2].textContent).toBe('  Description:  ');
+});
