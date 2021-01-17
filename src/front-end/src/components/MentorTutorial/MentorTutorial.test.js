3a949dfd07a8dc4fdc9293fd6c1b3eb78ab2d999
Aleks K <aleksschoool41@gmail.com>
2021-01-16 21:00:54 -0800

    updated based on erics pr

--- src/front-end/src/components/MentorTutorial/MentorTutorial.test.js
+++ src/front-end/src/components/MentorTutorial/MentorTutorial.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { render, unmountComponentAtNode } from 'react-dom';
 import { mockWindowMatchMedia } from '../../helper-methods';
-
 import MentorTutorial from './MentorTutorial';
 
 let container = null;
@@ -22,5 +21,5 @@ afterEach(() => {
 
 it('renders without crashing', () => {
   render(<MentorTutorial />, container);
-  expect(container.textContent).toBe('Reset your passwordEmailConfirm'); // test should fail
+  expect(container.textContent).toBe('Reset your passwordEmailConfirm'); // test should fail (but not give an error)
 });
