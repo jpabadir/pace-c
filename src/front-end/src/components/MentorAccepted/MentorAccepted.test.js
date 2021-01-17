1bdb5787fe7573e50ded705f089f40e5f2726563
Wrc980107 <61712799+Wrc980107@users.noreply.github.com>
2021-01-17 18:03:43 +0800

    Add tests to MentorAccepted(UNFINISHED)

---
+++ src/front-end/src/components/MentorAccepted/MentorAccepted.test.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { render, unmountComponentAtNode } from 'react-dom';
+import { mockWindowMatchMedia } from '../../helper-methods';
+
+import MentorAccepted from './MentorAccepted';
+
+let container = null;
+
+mockWindowMatchMedia();
+
+beforeEach(() => {
+  container = document.createElement('div');
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  // cleanup on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+test('renders without crashing', () => {
+  render(<MentorAccepted />, container);
+  expect(container.textContent).toBe('Accepted Mentees:Below you can view details about each accepted mentee',
+  );
+});
+
+test('MentorDisplay page is connected to this page', () => {
+  render(<MentorAccepted />, container);
+  const dis = document.createElement('MentorDisplay');
+  expect(container.contains(dis)).toBe(true);
+});
