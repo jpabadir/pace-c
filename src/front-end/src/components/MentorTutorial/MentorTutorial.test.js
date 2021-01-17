a5e786dda6a62f1472b183ae124384f525a6b025
Aleks K <aleksschoool41@gmail.com>
2021-01-17 09:25:38 -0800

    test 1 done

--- src/front-end/src/components/MentorTutorial/MentorTutorial.test.js
+++ src/front-end/src/components/MentorTutorial/MentorTutorial.test.js
@@ -21,5 +21,7 @@ afterEach(() => {
 
 it('renders without crashing', () => {
   render(<MentorTutorial />, container);
-  expect(container.textContent).toBe('Reset your passwordEmailConfirm'); // test should fail (but not give an error)
+  expect(container.textContent).toBe(
+    "Welcome To Mentor Match's Mentor Tutorial!This is the mentor tutorial page where you can learn how to see and interact with:Suggested MenteesAccepted MenteesSuggested Mentees:The suggested mentees tab is located on the top of the page. On the suggested mentee page you will be able to view all of your suggested mentees and details about each mentee. To accept a mentee click on the 'Accept ' button below the mentee's information. To decline a mentee click on the 'Decline' button below the mentee's information.Accepted Mentees:The accepted mentees tab is also located on the top of the page next to the suggested mentee tab. On the accepted mentees tab you can view the information of all mentees that have been accepted from the suggested mentees tab.",
+  ); // test should fail (but not give an error)
 });
