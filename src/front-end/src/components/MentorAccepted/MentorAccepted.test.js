0ce7249d64618c5b56a42b769e9c867ba4629988
wrc9817 <wrc9817@gmail.com>
2021-01-19 06:46:27 +0800

    delete useless line

--- src/front-end/src/components/MentorAccepted/MentorAccepted.test.js
+++ src/front-end/src/components/MentorAccepted/MentorAccepted.test.js
@@ -32,4 +32,3 @@ test('MentorDisplay page is connected to this page', () => {
   const dis = document.createElement('MentorDisplay');
   expect(container.contains(dis)).toBe(true);
 });
-// check which account I'm using to push 7
