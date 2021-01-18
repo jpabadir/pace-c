55610915e4a726abd88ec64b4763dfc35d082a41
Aleks K <aleksschoool41@gmail.com>
2021-01-17 20:59:56 -0800

    finished test

--- src/front-end/src/components/MentorTutorial/MentorTutorial.test.js
+++ src/front-end/src/components/MentorTutorial/MentorTutorial.test.js
@@ -19,9 +19,10 @@ afterEach(() => {
   container = null;
 });
 
+// method tests to see if the text on the mentor tutorial renders correctly
 it('renders without crashing', () => {
   render(<MentorTutorial />, container);
   expect(container.textContent).toBe(
     "Welcome To Mentor Match's Mentor Tutorial!This is the mentor tutorial page where you can learn how to see and interact with:Suggested MenteesAccepted MenteesSuggested Mentees:The suggested mentees tab is located on the top of the page. On the suggested mentee page you will be able to view all of your suggested mentees and details about each mentee. To accept a mentee click on the 'Accept ' button below the mentee's information. To decline a mentee click on the 'Decline' button below the mentee's information.Accepted Mentees:The accepted mentees tab is also located on the top of the page next to the suggested mentee tab. On the accepted mentees tab you can view the information of all mentees that have been accepted from the suggested mentees tab.",
-  ); // test should fail (but not give an error)
+  );
 });
