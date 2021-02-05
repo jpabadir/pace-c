bfa4cca6800d1947310e9989985bb1dc222adfee
Aleksander Konstantopoulos <aleksschoool41@gmail.com>
2021-02-05 03:12:45 -0800

    designed ManageMentors page

--- src/front-end/src/components/ManageMentors.js
+++ src/front-end/src/components/ManageMentors.js
@@ -1,68 +1,114 @@
 import React, { Component } from 'react';
-import { Button } from 'antd';
+import { Form, Input, Button } from 'antd';
 
 function requestMentor() {
-  // insert code to resend verification email
-  // or insert code to re-route to resend verification email page
-  window.alert('Verification Email Sent!');
+  // insert code to request mentors
+  window.alert('Your mentor request has been sent!');
 }
 
 // eslint-disable-next-line react/prefer-stateless-function
 class ManageMentors extends Component {
   render() {
     return (
-      <div className="EmailVerify">
-        <center>
+      <div className="ManageMentors" style={{ paddingTop: '10px' }}>
+        <div className="container">
           <h1>
-            <font size="6">
-              A verification email has been sent to your email address!
+            <font size="7">
+              <center> Manage and Invite Mentors </center>
             </font>
           </h1>
-        </center>
-        <br />
-        <center>
-          <h2>
-            <font size="5">
-              Please verify your email address in order to access:
-            </font>
-            <font size="4">
-              <ul>
-                <li>
-                  <i>Your Mentor Account</i>
-                </li>
-                <li>
-                  <i>The Mentor Tutorial</i>
-                </li>
-                <li>
-                  <i>Your Suggested Mentees</i>
-                </li>
-                <li>
-                  <i>Your Accepted Mentees</i>
-                </li>
-              </ul>
-            </font>
-          </h2>
-        </center>
-        <br />
-        <center>
-          <font size="5">
-            If you did not recieve a verification email or if the verification
-            email has already expired, click the button below to resend a
-            verification email.
-          </font>
-        </center>
-        <br />
-        <center>
-          <Button
-            onClick={requestMentor}
-            type="primary"
-            htmlType="submit"
-            className="formSubmitButton"
-            size="large"
-          >
-            Request Mentor
-          </Button>
-        </center>
+          <br />
+          <br />
+          <div id="CurrentActiveMentors">
+            <h1>
+              <center>
+                <i>
+                  <font size="5"> Current/Active Mentors: </font>
+                </i>
+              </center>
+            </h1>
+            <p>
+              <font size="4">
+                <center>
+                  **(under development)**
+                  <br />
+                  Insert/pull current/active mentors here.
+                </center>
+              </font>
+            </p>
+          </div>
+          <div id="InvitedPendingMentors">
+            <h1>
+              <center>
+                <i>
+                  <font size="5"> Invited/Pending Mentors: </font>
+                </i>
+              </center>
+            </h1>
+            <p>
+              <font size="4">
+                <center>
+                  **(under development)**
+                  <br />
+                  Insert/pull invited/pending mentors here.
+                </center>
+              </font>
+            </p>
+          </div>
+          <div id="InviteTab">
+            <h1>
+              <center>
+                <i>
+                  <font size="5"> Invite A Mentor! </font>
+                </i>
+              </center>
+            </h1>
+            <p>
+              <font size="4">
+                <center>
+                  <i> How To Invite A Mentor: </i>
+                  <br />
+                  To invite a user to join Mentor Match as a mentor that is
+                  associated with your organization, type their email address in
+                  the text box below and then click the &apos;request&apos;
+                  button.
+                  <br />
+                  <br />
+                  The mentor will then receive a link via email which will take
+                  them to Mentor Match&apos;s Mentor Form.
+                  <br />
+                  <br />
+                  <Form.Item
+                    label="Mentor's Email Address:"
+                    id="inviteMentorEmail"
+                    // must have an input:
+                    rules={[
+                      {
+                        required: true,
+                        message: "Please enter the mentor's email",
+                      },
+                    ]}
+                  >
+                    <Input
+                      onInput={this.handleNameChange}
+                      id="inviteMentorEmail"
+                      placeholder="Email Address"
+                    />
+                  </Form.Item>
+                  <Button
+                    onClick={requestMentor}
+                    type="primary"
+                    htmlType="submit"
+                    className="formSubmitButton"
+                    size="large"
+                  >
+                    Request Mentor
+                  </Button>
+                </center>
+              </font>
+            </p>
+          </div>
+        </div>
       </div>
     );
   }
