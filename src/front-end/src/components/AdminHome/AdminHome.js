f44376bf08be4ec1eb4ebb6503b18ae99e571909
Jean-Philippe Abadir <jpabadir@gmail.com>
2021-02-10 19:04:48 +0100

    Build admin home

--- src/front-end/src/components/AdminHome/AdminHome.js
+++ src/front-end/src/components/AdminHome/AdminHome.js
@@ -1,38 +1,56 @@
 import React, { Component } from 'react';
-import { Button } from 'antd';
-import { Route } from 'react-router-dom';
-import PlaceHolder from './PlaceHolder';
+import { Tabs } from 'antd';
+import {
+  CarryOutOutlined,
+  SolutionOutlined,
+  UsergroupAddOutlined,
+} from '@ant-design/icons';
+import ManageMentors from '../ManageMentors';
+import MenteeFormAndEmbedder from '../MenteeFormAndEmbedder/MenteeFormAndEmbedder';
+import MentorForm from '../MentorForm/MentorForm';
+
+const { TabPane } = Tabs;
 
 // eslint-disable-next-line react/prefer-stateless-function
 class AdminHome extends Component {
   render() {
     return (
       <>
-        <Button
-          type="primary"
-          htmlType="button"
-          id="mentorForm"
-          href="/placeHolder"
-        >
-          Mentor Form
-        </Button>
-        <Button
-          type="primary"
-          htmlType="button"
-          id="manageMentors"
-          href="/manage-mentors"
-        >
-          Manage Mentors
-        </Button>
-        <Button
-          type="primary"
-          htmlType="button"
-          id="menteeForm"
-          href="/placeHolder"
-        >
-          Mentee Form
-        </Button>
-        <Route path="/placeHolder" component={PlaceHolder} />
+        <Tabs defaultActiveKey="1">
+          <TabPane
+            key="1"
+            tab={
+              <span>
+                <SolutionOutlined />
+                Manage Mentors
+              </span>
+            }
+          >
+            <ManageMentors />
+          </TabPane>
+          <TabPane
+            key="2"
+            tab={
+              <span>
+                <UsergroupAddOutlined />
+                Manage Mentees
+              </span>
+            }
+          >
+            <MenteeFormAndEmbedder />
+          </TabPane>
+          <TabPane
+            key="3"
+            tab={
+              <span>
+                <CarryOutOutlined />
+                View Mentor Form
+              </span>
+            }
+          >
+            <MentorForm />
+          </TabPane>
+        </Tabs>
       </>
     );
   }
