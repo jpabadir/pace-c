c8cac5cb279756f000ca36a0ac083f9124eb28bd
Aleksander Konstantopoulos <aleksschoool41@gmail.com>
2021-03-12 03:33:33 -0800

    updated emailverify tests

--- src/front-end/src/components/EmailVerify/EmailVerify.test.js
+++ src/front-end/src/components/EmailVerify/EmailVerify.test.js
@@ -38,3 +38,27 @@ it('has a resend email button', () => {
   expect(container.contains(button)).toBe(true);
   expect(button.textContent).toBe('Resend Verification Email');
 });
+
+test('renders the correct header1 text', () => {
+  render(<EmailVerify />, container);
+  const h1 = document.getElementsByTagName('h1');
+  expect(h1[0].textContent).toBe(
+    'A verification email has been sent to your email address!',
+  );
+});
+
+test('renders the correct header2 text', () => {
+  render(<EmailVerify />, container);
+  const h2 = document.getElementsByTagName('h2');
+  expect(h2[0].textContent).toBe(
+    'Please verify your email address in order to access your account.',
+  );
+});
+
+test('renders the submit button correctly', () => {
+  render(<EmailVerify />, container);
+  const submitButton = document.getElementsByClassName(
+    'ant-btn ant-btn-primary ant-btn-lg formSubmitButton',
+  );
+  expect(submitButton[0].textContent).toBe('Resend Verification Email');
+});
