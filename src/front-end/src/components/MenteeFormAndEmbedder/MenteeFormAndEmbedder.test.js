04bc8ced7b8b44ed48cc4bc6a3e32341c44bb554
Aleksander Konstantopoulos <aleksschoool41@gmail.com>
2021-03-19 03:23:28 -0700

    iframe test completed

--- src/front-end/src/components/MenteeFormAndEmbedder/MenteeFormAndEmbedder.test.js
+++ src/front-end/src/components/MenteeFormAndEmbedder/MenteeFormAndEmbedder.test.js
@@ -41,3 +41,32 @@ test('renders the correct header2 text', () => {
     'The form below is an example of the mentee form:',
   );
 });
+
+function isInIframe() {
+  return window.top !== window.self;
+}
+
+// Referenced from: https://theunlikelydeveloper.com/jest-mock-window-test-iframes/
+describe('isInIframe', () => {
+  it('should return true if in iframe', () => {
+    // Extract the real properties so we can put them back after this test
+    const { top, self } = window;
+
+    // Delete the real properties from window so we can mock them
+    delete window.top;
+    delete window.self;
+
+    // Mock window.top and window.self as different objects (we don't
+    // need anything else from the original objects here, so {} is fine)
+    window.top = {};
+    window.self = {};
+
+    // Test the code
+    const result = isInIframe();
+    expect(result).toBe(true);
+
+    // Restore the original values so other tests will function normally
+    window.top = top;
+    window.self = self;
+  });
+});
