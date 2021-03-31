b38504c2786ada543bf89e5b93ce6118e7b630be
wrc9817 <wrc9817@gmail.com>
2021-03-31 19:14:08 +0800

    Address comments

--- src/front-end/src/components/MentorSuggested/MentorSuggested.js
+++ src/front-end/src/components/MentorSuggested/MentorSuggested.js
@@ -43,7 +43,8 @@ class MentorSuggested extends Component {
 
       // Confirm with the user that this was successful
       notification.success({
-        message: 'Mentee request accepted!',
+        message:
+          "Mentee request accepted! The mentee will be notified of their acceptance through email. You can view their email address from the 'Accepted Mentees' tab if you'd like to email them",
       });
     });
   }
